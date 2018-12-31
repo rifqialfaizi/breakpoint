8889e640b5b4e2bd14dcdeccc8ea2954e705884d
Me <rifqialfaizii@gmail.com>
2018-12-31 15:06:43 +0700

    Initial Commit

---
+++ breakpoint/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  breakpoint
+//
+//  Created by Rifqi Alfaizi on 31/12/18.
+//  Copyright © 2018 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
